212fc6042753388395d505c64763c09603cd866c
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2022-09-29 17:01:32 -0300

    att
    att

--- src/domain/general/templates/footer.tpl.php
+++ src/domain/general/templates/footer.tpl.php
@@ -2,6 +2,6 @@
 
 
 <div class="footer">
-    <a href="https://mezap.com.br/" target="_blank">Mod por Mezap</a> - <?=$this->get("version");?></p>
+    <a href="http://leantime.io" target="_blank">Powered By Leantime</a> - <?=$this->get("version");?></p>
 </div>
 
--- src/domain/general/templates/loginInfo.tpl.php
+++ src/domain/general/templates/loginInfo.tpl.php
@@ -51,7 +51,17 @@
             </a>
         </li>
         <li>
-            <a href='https://mezap.com.br/' target="_blank">
+            <a href='https://docs.leantime.io' target="_blank">
+                <?=$this->__("menu.knowledge_base")?>
+            </a>
+        </li>
+        <li>
+            <a href='https://community.leantime.io' target="_blank">
+                <?=$this->__("menu.community")?>
+            </a>
+        </li>
+        <li>
+            <a href='https://leantime.io/contact-us' target="_blank">
                 <?=$this->__("menu.contact_us")?>
             </a>
         </li>
